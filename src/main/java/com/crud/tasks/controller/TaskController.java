248a4ecbb5a92f16275dc910f9ecba9158ca0a47
Artur Borkowski <arturborkowski7@gmail.com>
2020-07-21 20:51:17 +0200

    task 18.4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -16,18 +16,22 @@ public class TaskController {
         return new ArrayList<>();
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public TaskDto getTask(Long taskId){
         return new TaskDto(1L, "test title", "test_content");
     }
 
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId){
 
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto){
         return new TaskDto(1L, "Edited test title", "Test content");
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto){
 
     }
