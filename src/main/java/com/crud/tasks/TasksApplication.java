36a8d6ddaf8e509ef5f47017402ec760fdb025dc
Artur Borkowski <arturborkowski7@gmail.com>
2020-09-13 21:28:13 +0200

    Heroku

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TasksApplication {
+public class TasksApplication {//extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
+
+	//@Override
+	//protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	//	return builder.sources(TasksApplication.class);
+	//}
 }
